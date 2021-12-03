b93b3ffb5598f81185f884d066bd6165afc4436c
Mike Kelly <mike.forge@gmail.com>
2021-12-02 22:48:29 -0500

    playing with some bootstrap

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+var currentDayEl = $('#currentDay');
+
+
+
+var date = moment().format('dddd, MMM Do YYYY');
+currentDayEl.html(date);
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Daily-Planner
+
+This is a daily planner allowing the user to save events for each hour if desired.
